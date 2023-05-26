fab05582a7a7d2f8d9bef4649be6de2c71f710f2
Matthew Lim <matlim200@gmail.com>
2023-05-25 18:01:04 -0700

    Add #render tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,51 +3,74 @@ require './lib/cell'
 
 RSpec.describe Cell do
   before do
-    @cell = Cell.new("B4")
+    @cell_1 = Cell.new("B4")
+    @cell_2 = Cell.new("C3")
     @cruiser = Ship.new("Cruiser", 3)
   end
 
   describe "#exists" do
     it "exists" do
-      expect(@cell).to be_a(Cell)
+      expect(@cell_1).to be_a(Cell)
     end
 
     it "has readable attributes" do
-      expect(@cell.coordinates).to eq("B4")
-      expect(@cell.ship).to eq(nil)
+      expect(@cell_1.coordinates).to eq("B4")
+      expect(@cell_1.ship).to eq(nil)
     end
   end
 
   describe "#empty?" do
     it "can check if a cell is empty" do
-      expect(@cell.empty?).to eq(true)
+      expect(@cell_1.empty?).to eq(true)
     end
   end
 
   describe "#place_ship" do
     it "can place a ship within a cell" do
-      @cell.place_ship(@cruiser)
+      @cell_1.place_ship(@cruiser)
 
-      expect(@cell.ship).to eq(@cruiser)
-      expect(@cell.empty?).to eq(false)
+      expect(@cell_1.ship).to eq(@cruiser)
+      expect(@cell_1.empty?).to eq(false)
     end
   end
 
   describe "#fired_upon?" do
     it "can check if it has been fired at" do
-      @cell.place_ship(@cruiser)
-
-      expect(@cell.fired_upon?).to eq(false)
+      @cell_1.place_ship(@cruiser)
+      # require 'pry'; binding.pry
+      expect(@cell_1.fired_upon?).to eq(false)
     end
   end
 
   describe "fire_upon" do
     it "can subtract 1 health from the ship when it is fired upon" do
-      @cell.place_ship(@cruiser)
-      @cell.fire_upon
+      @cell_1.place_ship(@cruiser)
+      @cell_1.fire_upon
 
-      expect(@cell.ship.health).to eq(2)
-      expect(@cell.fire_upon?).to eq(true)
+      expect(@cell_1.ship.health).to eq(2)
+      expect(@cell_1.fired_upon?).to eq(true)
+    end
+  end
+
+  describe "#render" do
+    it "renders . if the cell has not been fired upon" do
+      @cell_1.place_ship(@cruiser)
+
+      expect(@cell_1.render).to eq(".")
+    end
+
+    it "renders M if the cell has been fired upon and it does not contain a ship" do
+      @cell_1.fire_upon
+
+      expect(@cell_1.render).to eq("M")
+    end
+
+    it "renders H if the cell has been fired upon and it contains a ship (the shot was a hit)." do
+
+    end
+
+    xit "renders “X” if the cell has been fired upon and its ship has been sunk. " do
+
     end
   end
 end
