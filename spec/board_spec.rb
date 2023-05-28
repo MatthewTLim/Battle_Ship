c27be4d866dbaad37012f43f3db6bd6b65585673
Terry Lamb <terrylamb@gmail.com>
2023-05-28 14:29:16 -0400

    complete render and render_assistant tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -110,21 +110,20 @@ RSpec.describe Board do
 
   describe "#render" do
     it "can render a String representation of itself to display to the user all of its cells in a formatted grid" do
-      @board.place(@cruiser, ["A1", "A2", "A3"])
 
       expect(@board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    end
+
+    it "can render ships on the board when option is true" do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+
+      expect(@board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
     end
   end
 
   describe "#render_assistant" do
     it "can add renderable spaces to board i.e.(., M, S, H, X)" do
       expect(@board.render_assistant).to eq([". . . .", ". . . .", ". . . .", ". . . ."])
-    end
-  end
-
-  describe "#letter_adder" do
-    xit "can add letters to the board" do
-      expect(@board.letter_adder).to eq(["A. . . . . . . . . . . . . . . .", "B. . . . . . . . . . . . . . . .", "C. . . . . . . . . . . . . . . .", "D. . . . . . . . . . . . . . . ."])
     end
   end
 end
