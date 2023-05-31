333e7d933faab4631bb1e620dd4b2196a0c7bfcf
Matthew Lim <matlim200@gmail.com>
2023-05-31 12:15:09 -0700

    Edit random placement tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -37,12 +37,14 @@ RSpec.describe Board do
   describe "#valid_placement?" do
     it "can check if coordinates in the array are the same as the length of the ship" do
       expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to eq(true)
       expect(@board.valid_placement?(@submarine, ["A1", "A3", "A4"])).to eq(false)
       # Add fringe tests here later
     end
 
     it "can make sure the coordinates are consecutive" do
       expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to eq(true)
       expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
       # Add fringe tests here later
     end
@@ -50,6 +52,7 @@ RSpec.describe Board do
     it "can make sure a ship will not be placed diagonally" do
       expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to eq(false)
       expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to eq(false)
+      expect(@board.valid_placement?(@cruiser, ["A3", "B4", "D2"])).to eq(false)
     end
 
     it "will not place a ship if the cell already contains a ship" do
@@ -66,7 +69,9 @@ RSpec.describe Board do
     describe "#consecutive_check" do
       it "can tell if the coordinates are consecutive" do
         coordinates = ["A1", "A2", "A3"]
+        coordinates2 = ["A1", "A4", "C4"]
         expect(@board.consecutive_check(coordinates)).to eq(true)
+        expect(@board.consecutive_check(coordinates2)).to eq(false)
       end
     end
 
@@ -74,7 +79,7 @@ RSpec.describe Board do
       it "can tell if the cooridnates are diagonal from one another" do
         valid_coordinate = ["A1", "B2", "C3"]
         expect(@board.diagonal_check(valid_coordinate)).to eq(true)
-    end
+     end
     end
 
     describe "#ordinal_extractor" do
@@ -88,11 +93,15 @@ RSpec.describe Board do
       it "can check if a cell contains a ship" do
         overlapped_coordinates = ["A1", "A2", "A3"]
         test_coordinates = ["A1", "A2", "A4"]
+        good_coords = ["B2", "B3", "B4"]
 
-        @board.place(@cruiser, ["A1", "A2", "A3"])
+        @board.place(@cruiser, overlapped_coordinates)
 
+        # expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to eq(false)
+        # expect(@board.valid_placement?(@submarine, ["B1", "B2"])).to eq(true)
         expect(@board.overlap_check(overlapped_coordinates)).to eq(true)
         expect(@board.overlap_check(test_coordinates)).to eq(true)
+        expect(@board.overlap_check(good_coords)).to eq(false)
       end
     end
   end
@@ -124,6 +133,17 @@ RSpec.describe Board do
   describe "#render_assistant" do
     it "can add renderable spaces to board i.e.(., M, S, H, X)" do
       expect(@board.render_assistant).to eq([". . . .", ". . . .", ". . . .", ". . . ."])
+    end
+  end
+
+  describe "#randomly_place" do
+    it "can randomly place ships in cells" do
+      place = @board.randomly_place(@cruiser)
+
+      expect(place.length).to eq(3)
+      expect(@board.cells[place[0]].ship).to eq(@cruiser)
+      expect(@board.cells[place[1]].ship).to eq(@cruiser)
+      expect(@board.cells[place[2]].ship).to eq(@cruiser)
     end
   end
 end
